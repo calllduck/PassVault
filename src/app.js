d220ae8db2867390ae4362c99d395682c619e8c3
calllduck <kettin@binus.ac.id>
2026-06-14 01:20:56 +0700

    fix: add trust proxy for Railway reverse proxy

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-app.set('trust proxy', 1);
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+app.set('trust proxy', 1);
 const authRoutes = require('./routes/auth');
 const vaultRoutes = require('./routes/vault');
 
